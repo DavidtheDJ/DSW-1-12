8e0f2ffd2d590d881a47571bb26f96793c96d554
DavidtheDJ <DavidtheDJ00@gmail.com>
2017-01-18 18:32:19 -0800

    finished

---
+++ Field_Sim/field_manual_grow_dialog_class.py
@@ -0,0 +1,50 @@
+#David Justice
+#1-12-17
+#
+
+from PyQt4.QtGui import *
+
+class ManualGrowDialog(QDialog):
+    """this class creates a dialog to provide light,water and food values"""
+
+    def __init__(self):
+        super().__init__()
+
+        #creates widgets
+        self.water_spin_box = QSpinBox()
+        self.light_spin_box = QSpinBox()
+        self.food_spin_box = QSpinBox()
+
+        self.submit_button = QPushButton("Manually grow field")
+
+        #set-up spinboxes
+        self.water_spin_box.setRange(0,10)
+        self.light_spin_box.setRange(0,10)
+        self.food_spin_box.setRange(0,10)
+
+        self.water_spin_box.setSuffix(" Water")
+        self.light_spin_box.setSuffix(" Light")
+        self.food_spin_box.setSuffix(" Food")
+
+        self.water_spin_box.setValue(1)
+        self.light_spin_box.setValue(1)
+        self.food_spin_box.setValue(1)
+
+        #create layout
+        self.layout = QVBoxLayout()
+
+        #add widgets to the layout
+        self.layout.addWidget(self.light_spin_box)
+        self.layout.addWidget(self.water_spin_box)
+        self.layout.addWidget(self.food_spin_box)
+        self.layout.addWidget(self.submit_button)
+
+        #set the window layout
+        self.setLayout(self.layout)
+
+        #connections
+        self.submit_button.clicked.connect(self.close)
+
+    def get_values(self):
+        return self.light_spin_box.value(), self.water_spin_box.value(), self.food_spin_box.value()
+        
---
+++ Field_Sim/graphic_sheep_item_class.py
@@ -0,0 +1,20 @@
+#David Justice
+#12-14-16
+#sheep pixmap
+
+from graphic_animal_item_class import *
+from sheep_class import *
+
+import field_resources
+
+class SheepGraphicsPixmapItem(AnimalGraphicsPixmapItem):
+    """this class provides a graphical represantation of a sheep"""
+
+    #constructor
+    def __init__(self):
+        self.available_graphics = [":/sheep_baby.png", ":/sheep_poor.png",
+                                   ":/sheep_fine.png", ":/sheep_prime.png"]
+
+        super().__init__(self.available_graphics)
+
+        self.animal = Sheep("")
